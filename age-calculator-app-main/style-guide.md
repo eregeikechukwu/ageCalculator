0160045a7cb8e4d1aa1ea172791addf8413bc6e8
Erege Nelson <eregeikechukwu@gmail.com>
2024-05-24 07:49:06 +0100

    first commit

---
+++ age-calculator-app-main/style-guide.md
@@ -0,0 +1,146 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+> 💡 These are just the design sizes. Ensure content is responsive and meets WCAG requirements by testing the full range of screen sizes from 320px to large screens.
+
+## Colors
+
+### Primary
+
+- Purple: hsl(259, 100%, 65%)
+- Light red: hsl(0, 100%, 67%)
+
+### Neutral
+
+- White: hsl(0, 0%, 100%)
+- Off white: hsl(0, 0%, 94%)
+- Light grey: hsl(0, 0%, 86%)
+- Smokey grey: hsl(0, 1%, 44%)
+- Off black: hsl(0, 0%, 8%)
+
+## Typography
+
+### Body Copy
+
+- Font size (inputs): 32px
+
+### Font
+
+- Family: [Poppins](https://fonts.google.com/specimen/Poppins)
+- Weights: 400i, 700, 800i
+
+> 💎 [Upgrade to Pro](https://www.frontendmentor.io/pro?ref=style-guide) for design file access to see all design details and get hands-on experience using a professional workflow with tools like Figma.
+
+.poppins-thin {
+font-family: "Poppins", sans-serif;
+font-weight: 100;
+font-style: normal;
+}
+
+.poppins-extralight {
+font-family: "Poppins", sans-serif;
+font-weight: 200;
+font-style: normal;
+}
+
+.poppins-light {
+font-family: "Poppins", sans-serif;
+font-weight: 300;
+font-style: normal;
+}
+
+.poppins-regular {
+font-family: "Poppins", sans-serif;
+font-weight: 400;
+font-style: normal;
+}
+
+.poppins-medium {
+font-family: "Poppins", sans-serif;
+font-weight: 500;
+font-style: normal;
+}
+
+.poppins-semibold {
+font-family: "Poppins", sans-serif;
+font-weight: 600;
+font-style: normal;
+}
+
+.poppins-bold {
+font-family: "Poppins", sans-serif;
+font-weight: 700;
+font-style: normal;
+}
+
+.poppins-extrabold {
+font-family: "Poppins", sans-serif;
+font-weight: 800;
+font-style: normal;
+}
+
+.poppins-black {
+font-family: "Poppins", sans-serif;
+font-weight: 900;
+font-style: normal;
+}
+
+.poppins-thin-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 100;
+font-style: italic;
+}
+
+.poppins-extralight-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 200;
+font-style: italic;
+}
+
+.poppins-light-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 300;
+font-style: italic;
+}
+
+.poppins-regular-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 400;
+font-style: italic;
+}
+
+.poppins-medium-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 500;
+font-style: italic;
+}
+
+.poppins-semibold-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 600;
+font-style: italic;
+}
+
+.poppins-bold-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 700;
+font-style: italic;
+}
+
+.poppins-extrabold-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 800;
+font-style: italic;
+}
+
+.poppins-black-italic {
+font-family: "Poppins", sans-serif;
+font-weight: 900;
+font-style: italic;
+}
---
+++ script.js
@@ -0,0 +1,153 @@
+'use strict';
+
+const iday = document.querySelector('.day');
+const iMonth = document.querySelector('.month');
+const iYear = document.querySelector('.year');
+const mButton = document.querySelector('.btn');
+const oDay = document.querySelector('.oDays');
+const oMonth = document.querySelector('.oMonths');
+const oYear = document.querySelector('.oYears');
+
+const errED = document.querySelector('.errED');
+const errID = document.querySelector('.errID');
+
+const errEM = document.querySelector('.errEM');
+const errIM = document.querySelector('.errIM');
+
+const errEY = document.querySelector('.errEY');
+const errIY = document.querySelector('.errIY');
+
+const lD = document.querySelector('.lD');
+const lM = document.querySelector('.lM');
+const lY = document.querySelector('.lY');
+
+let day;
+let month;
+let year;
+
+const calculateAge = function () {
+  //Condition for the day validation
+  if (
+    iday.value === '' ||
+    isNaN(+iday.value) ||
+    +iday.value < 1 ||
+    +iday.value > 31
+  ) {
+    iday.style.borderColor = 'hsl(0, 100%, 67%)';
+    lD.style.color = 'hsl(0, 100%, 67%)';
+
+    if (iday.value === '') {
+      errED.style.display = 'inline';
+      errID.style.display = 'none';
+    } else {
+      errID.style.display = 'inline';
+      errED.style.display = 'none';
+    }
+  } else {
+    errED.style.display = 'none';
+    errID.style.display = 'none';
+    iday.style.borderColor = '';
+    lD.style.color = '';
+
+    day = +iday.value;
+  }
+
+  //Condition for Month Validation
+  if (
+    iMonth.value === '' ||
+    isNaN(+iMonth.value) ||
+    +iMonth.value < 1 ||
+    +iMonth.value > 12
+  ) {
+    iMonth.style.borderColor = 'hsl(0, 100%, 67%)';
+    lM.style.color = 'hsl(0, 100%, 67%)';
+
+    if (iMonth.value === '') {
+      errEM.style.display = 'inline';
+      errIM.style.display = 'none';
+    } else {
+      errIM.style.display = 'inline';
+      errEM.style.display = 'none';
+    }
+  } else {
+    errEM.style.display = 'none';
+    errIM.style.display = 'none';
+    iMonth.style.borderColor = '';
+    lM.style.color = '';
+
+    month = +iMonth.value - 1;
+  }
+
+  //Condition for Year Validation
+  if (
+    iYear.value === '' ||
+    isNaN(+iYear.value) ||
+    +iYear.value < 1 ||
+    +iYear.value > new Date().getFullYear()
+  ) {
+    iYear.style.borderColor = 'hsl(0, 100%, 67%)';
+    lY.style.color = 'hsl(0, 100%, 67%)';
+
+    if (iYear.value === '') {
+      errEY.style.display = 'inline';
+      errIY.style.display = 'none';
+    } else {
+      errIM.style.display = 'inline';
+      errEY.style.display = 'none';
+    }
+  } else {
+    errEY.style.display = 'none';
+    errIY.style.display = 'none';
+    iYear.style.borderColor = '';
+    lY.style.color = '';
+
+    year = +iYear.value;
+  }
+  console.log(year, month, day);
+  ////////////////
+  //Master condition
+  let age;
+  if (year && month && day) {
+    console.log(year, month, day);
+    age = new Date(year, month, day);
+
+    console.log(age);
+    const ageS = age.getTime();
+    console.log(ageS);
+    const nowS = Date.now();
+
+    const tD = nowS - ageS;
+    console.log(tD);
+
+    const cD = new Date(tD);
+    console.log(cD);
+
+    const yearI = cD.getFullYear();
+    const monthZ = cD.getMonth();
+    const dateZ = cD.getDate();
+
+    console.log(yearI, monthZ, dateZ);
+
+    const yearZ = yearI - 1970;
+
+    oDay.textContent = dateZ;
+    oMonth.textContent = monthZ;
+    oYear.textContent = yearZ;
+
+    console.log(
+      `You have lived for ${yearZ} years,  ${
+        monthZ > 1 ? monthZ + ' months' : 'a month'
+      }  and ${dateZ > 1 ? dateZ + ' days' : 'a day'}!!!`
+    );
+  } else {
+    return;
+  }
+
+  //////////////////
+  //Functionality
+};
+
+mButton.addEventListener('click', calculateAge);
+// const dec = 'April 1 2007 ';
+// console.log(dec);
+// console.log(calculateAge());
